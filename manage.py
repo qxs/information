d581b4625a1a8f215b9037c296fe8acf2679610f
qxs <939727234@qq.com>
2018-05-25 15:08:45 +0800

    配置文件的加载

---
+++ manage.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+class Config(object):
+    DEBUG =True
+
+app.config.from_object(Config)
+
+@app.route("/index")
+def index():
+    return "index"
+
+if __name__ == '__main__':
+    app.run()
