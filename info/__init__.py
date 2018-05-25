5c2e2fa42bae79ed68051725f5c2024938856a85
qxs <939727234@qq.com>
2018-05-25 18:05:36 +0800

    全局db的处理

--- info/__init__.py
+++ info/__init__.py
@@ -3,14 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
-from config import Config,ProductionConfig,DevlopmentConfig,UnittestConfig
 from config import configs
 
+
+db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(configs[config_name])
-
     db = SQLAlchemy(app)
+    db.init_app(app)
     redis_store = StrictRedis(host=configs[config_name].REDIS_HOST,port=configs[config_name].REDIS_PORT)
     CSRFProtect(app)
     Session(app)
--- manage.py
+++ manage.py
@@ -1,12 +1,12 @@
 from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
 # from info import app,db
-from info import create_app
+from info import create_app,db
 
 
 app = create_app('dev')
 manager = Manager(app)
-# Migrate(app,db)
+Migrate(app,db)
 manager.add_command('mysql',MigrateCommand)
 
 @app.route("/")
