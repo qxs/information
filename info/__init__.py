5256753556915d66c35494cbe474eb65bc12d610
qxs <939727234@qq.com>
2018-05-25 16:31:40 +0800

    config配置信息的封装到config.py中

---
+++ info/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
+from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
+from config import Config
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
+CSRFProtect(app)
+Session(app)
