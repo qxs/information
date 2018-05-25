bcbcf815a150d708c9d14abdbf1fa5399168a067
qxs <939727234@qq.com>
2018-05-25 15:56:08 +0800

    Session的配置

--- manage.py
+++ manage.py
@@ -2,25 +2,36 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
 
 app = Flask(__name__)
 
 class Config(object):
-    DEBUG =True
-
+    DEBUG = True
+    SECRET_KEY ='ddsfggfgggrffsddddfrfrhhhlsdfklkfoejonnjowwehfwofkwmklfsflnfwo'
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306:information'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
+    # 指定session使用什么来存储
+    SESSION_TYPE = 'redis'
+    # 指定session数据存储在后端的位置
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 是否使用secret_key签名你的sessin
+    SESSION_USE_SIGNER = True
+    # 设置过期时间，要求'SESSION_PERMANENT', True。而默认就是31天
+    PERMANENT_SESSION_LIFETIME = 60 * 60 * 24  # 一天有效期
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 CSRFProtect(app)
+Session(app)
 
 @app.route("/")
 def index():
-    redis_store.set('name','wfy')
+    from flask import session
+    session['age'] = 18
     return "index"
 
 if __name__ == '__main__':
