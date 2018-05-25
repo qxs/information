4a84db511ecfdb96c04b8d1fe4eeece4a7a8e7a0
qxs <939727234@qq.com>
2018-05-25 16:30:43 +0800

    抽取app到业务逻辑模块ｉｎｆｏ下

--- manage.py
+++ manage.py
@@ -1,20 +1,8 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from redis import StrictRedis
-from flask_wtf.csrf import CSRFProtect
-from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
-from config import Config
+from info import app,db
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
-CSRFProtect(app)
-Session(app)
 manager = Manager(app)
 Migrate(app,db)
 manager.add_command('mysql',MigrateCommand)
