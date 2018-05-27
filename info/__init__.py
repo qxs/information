2156e19b77593fff838c4028b20636f52ccdfd7f
qxs <939727234@qq.com>
2018-05-27 09:21:14 +0800

    整理

--- info/__init__.py
+++ info/__init__.py
@@ -1,11 +1,12 @@
-from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
+# from config import Config, DevlopmentConfig, ProductionConfig, UnittestConfig
 from config import configs
 import logging
+from logging.handlers import RotatingFileHandler
 
 
 def setup_log(level):
@@ -22,15 +23,34 @@ def setup_log(level):
     logging.getLogger().addHandler(file_log_handler)
 
 
+# 创建SQLAlchemy对象
 db = SQLAlchemy()
+
+
 def create_app(config_name):
+    """创建app的工厂方法
+    参数：根据参数选择不同的配置类
+    """
+
     # 根据创建app时的配置环境，加载日志等级
     setup_log(configs[config_name].LEVEL_LOG)
+
     app = Flask(__name__)
+
+    # 获取配置信息
     app.config.from_object(configs[config_name])
-    db = SQLAlchemy(app)
+
+    # 创建连接到MySQL数据库的对象
+    # db = SQLAlchemy(app)
     db.init_app(app)
-    redis_store = StrictRedis(host=configs[config_name].REDIS_HOST,port=configs[config_name].REDIS_PORT)
+
+    # 创建连接到redis数据库的对象
+    redis_store = StrictRedis(host=configs[config_name].REDIS_HOST, port=configs[config_name].REDIS_PORT)
+
+    # 开启CSRF保护：因为项目中的表单不再使用FlaskForm来实现，所以不会自动的开启CSRF保护，需要自己开启
     CSRFProtect(app)
+
+    # 指定session数据存储在后端的位置
     Session(app)
+
     return app
--- manage.py
+++ manage.py
@@ -1,21 +1,34 @@
 from flask_script import Manager
-from flask_migrate import Migrate,MigrateCommand
-# from info import app,db
-from info import create_app,db
+from flask_migrate import Migrate, MigrateCommand
+# from info import app, db
+from info import create_app, db
 
 
+# 创建app
 app = create_app('dev')
-manager = Manager(app)
-Migrate(app,db)
-manager.add_command('mysql',MigrateCommand)
 
-@app.route("/")
+# 创建脚本管理器对象
+manager = Manager(app)
+# 让迁移和app和数据库建立管理
+Migrate(app, db)
+# 将数据库迁移的脚本添加到manager
+manager.add_command('mysql', MigrateCommand)
+
+
+@app.route('/')
 def index():
-    from flask import session
-    session['age'] = 18
-    return "index"
+
+    # 测试redis数据库
+    # redis_store.set('name', 'zxj')
+
+    # 测试session
+    # from flask import session
+    # 会将{'age':'2'}写入到cookie
+    # session['age'] = '2'
+
+    return 'index'
+
 
 if __name__ == '__main__':
+
     manager.run()
-
-
