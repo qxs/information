a6d81b60f18844591f07b6515b2961fff1b2a518
qxs <939727234@qq.com>
2018-05-27 22:30:50 +0800

    渲染主页

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,8 @@
 from . import index_blue
 from info import redis_store
+from flask import render_template
 
 @index_blue.route('/')
 def index():
-    redis_store.set('name','qxs')
-    return 'index'
+
+    return render_template('news/index.html')
