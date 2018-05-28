fbdd152b7f07a69b318a33910fdbf66c50ccc5e6
qxs <939727234@qq.com>
2018-05-28 09:19:24 +0800

    图片验证后端前端逻辑

--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -1,7 +1,22 @@
 from . import passport_blue
-from flask import request
+from flask import request,abort,current_app,make_response
+from  info.utils.captcha.captcha import captcha
+from  info import redis_store,constants
+
 
 @passport_blue.route('/image_code',methods=['GET'])
 def image_code():
-    print(request.url)
-    # return ''
+    imageCodeId = request.args.get('imageCodeId')
+    if not imageCodeId:
+        abort(403)
+
+    name,text,image = captcha.generate_captcha()
+    try:
+        redis_store.set('imageCoedId:'+imageCodeId,text,constants.IMAGE_CODE_REDIS_EXPIRES)
+    except Exception as e:
+        current_app.app.logging.error(e)
+        abort(500)
+    response = make_response(image)
+    response.headers['Content-Type']='image/jpg'
+    return response
+
