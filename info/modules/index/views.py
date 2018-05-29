c70f4e1023f495daa4a24f2a51c056ed95ea11fb
qxs <939727234@qq.com>
2018-05-29 19:39:21 +0800

    登录右上角用户信息显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,11 +1,26 @@
 from . import index_blue
 from info import redis_store
-from flask import render_template,current_app
+from flask import render_template,current_app,session
+from info.models import User
 
 
 @index_blue.route('/')
 def index():
-    return render_template('news/index.html')
+    '''登录
+    １　处理右上角的登录或者注册显示'''
+    user_id = session.get('user_id',None)
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
+    context = {
+        'user':user
+    }
+
+    return render_template('news/index.html',context = context)
 
 
 @index_blue.route('/favicon.ico',methods=['GET'])
