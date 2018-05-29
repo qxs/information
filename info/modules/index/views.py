c5245c9219fe85709b31c17613f26b417ed7e08f
qxs <939727234@qq.com>
2018-05-29 22:23:26 +0800

    新闻点击排行的展示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,13 +1,17 @@
 from . import index_blue
 from info import redis_store
 from flask import render_template,current_app,session
-from info.models import User
+from info.models import User,News
+from info import constants
 
 
 @index_blue.route('/')
 def index():
     '''登录
-    １　处理右上角的登录或者注册显示'''
+    １　处理右上角的登录或者注册显示
+    2 新闻点击排行展示，在ｎｅｗｓ数据库中查询，根据点击量ｃｌｉｃｋｓ查询 倒序
+    '''
+
     user_id = session.get('user_id',None)
     user = None
     if user_id:
@@ -16,8 +20,16 @@ def index():
         except Exception as e:
             current_app.logger.error(e)
 
+    # 2 新闻点击排行展示
+    news_clicks = []
+    try:
+        news_clicks = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+
     context = {
-        'user':user
+        'user':user,
+        'news_clicks':news_clicks
     }
 
     return render_template('news/index.html',context = context)
