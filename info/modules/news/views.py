bed7c7662b185236343f2cd58a3b5108f6049886
qxs <939727234@qq.com>
2018-06-01 23:02:09 +0800

    新闻收藏和取消收藏

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,10 +1,51 @@
 from . import news_blue
-'''新闻详情：收藏,评论，点赞，'''
-from flask import render_template,session,current_app,abort,g
+from flask import render_template,session,current_app,abort,g,jsonify,request
 from info.models import News,User,constants,Category,db
 from info.utils.comment import user_login_data
+from info import response_code
+'''新闻详情：收藏,评论，点赞，'''
 
+@news_blue.route('/news_collect',methods=['POST'] )
+@user_login_data
+def news_collect():
+    """新闻收藏和取消收藏
+        １获取登录用户信息"""
+    user = g.user
+    if not user:
+        return jsonify(errno=response_code.RET.SESSIONERR,errmsg='用户未登录' )
+    news_id = request.json.get('news_id')
+    action =request.json.get('action')
 
+    if not all([news_id,action]):
+        return jsonify(errno=response_code.RET.PARAMERR, errmsg='缺少参数')
+    if action not in ['collect','cancel_collect']:
+        return jsonify(errno=response_code.RET.PARAMERR, errmsg='参数错误')
+
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=response_code.RET.NODATA, errmsg='新闻不存在')
+
+    if not news:
+        return jsonify(errno=response_code.RET.PARAMERR, errmsg='缺少参数')
+
+    if action == "collect":
+        # 收藏新闻
+        user.collection_news.append(news)
+    else:
+        if news in user.collection_news:
+            # 取消收藏新闻
+            user.collection_news.remove(news)
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=response_code.RET.DBERR, errmsg='操作失败')
+
+    return jsonify(errno=response_code.RET.OK, errmsg='操作成功')
 
 @news_blue.route('/detail/<int:news_id>')
 @user_login_data
--- info/utils/comment.py
+++ info/utils/comment.py
@@ -1,6 +1,7 @@
 #公共的工具文件
 from flask import session,current_app,g
 from info.models import User
+from functools import wraps
 
 
 def do_rank(index):
@@ -15,6 +16,7 @@ def do_rank(index):
 
 
 def user_login_data(view_func):
+    @wraps(view_func)
     def wrapper(*args,**kwargs):
         user_id = session.get('user_id', None)
         user = None
