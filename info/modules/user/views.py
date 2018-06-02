7ebea75aa066be6efe276a2b30332150e51bf647
qxs <939727234@qq.com>
2018-06-03 00:59:15 +0800

    个人中心基本资料修改前后端逻辑

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -1,23 +1,49 @@
 '''个人中心'''
 from . import user_blue
-from  flask import render_template,g,current_app,jsonify,url_for,redirect
+from  flask import render_template,g,current_app,jsonify,url_for,redirect,request,session
 from info.utils.comment import user_login_data
-from info import response_code
+from info import response_code,db
 
 
 @user_blue.route('/base_info',methods=['GET','POST'])
 @user_login_data
 def base_info():
     # 1 获取登录用户信息
+
     user = g.user
     if not user:
-        return redirect(url_for('index.index'))
+            return redirect(url_for('index.index'))
+    #实现ｇｅｔ请求逻辑
+    if request.method == 'GET':
+        context = {
+            'user': user
+        }
+        #渲染界面
+        return render_template('news/user_base_info.html',context=context)
+    #实现ｐｏｓｔ请求逻辑
+    if request.method == 'POST':
+        nick_name = request.json.get('nick_name')
+        signature = request.json.get('signature')
+        gender = request.json.get('gender')
 
-    context = {
-        'user': user
-    }
+        if not all([nick_name,signature,gender]):
+            return jsonify(errno=response_code.RET.PARAMERR, errmsg='缺少参数')
+        if gender not in ['MAN','WOMAN']:
+            return jsonify(errno=response_code.RET.PARAMERR, errmsg='参数错误')
 
-    return render_template('news/user_base_info.html',context=context)
+        user.nick_name = nick_name
+        user.signature = signature
+        user.gender = gender
+        try:
+            db.session.commit()
+        except Exception as e:
+            current_app.logger.error(e)
+            db.session.rollback()
+            return jsonify(errno=response_code.RET.DBERR, errmsg='保存失败')
+
+        session.nick_name = nick_name
+
+    return jsonify(errno=response_code.RET.OK, errmsg='OK')
 
 
 @user_blue.route('/info',methods = ['GET','POST'])
