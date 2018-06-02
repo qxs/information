6c36fe55e3d1e86863ba80eedcf0687f77967198
qxs <939727234@qq.com>
2018-06-02 23:54:38 +0800

    基本资料查询和展示

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -5,6 +5,21 @@ from info.utils.comment import user_login_data
 from info import response_code
 
 
+@user_blue.route('/base_info',methods=['GET','POST'])
+@user_login_data
+def base_info():
+    # 1 获取登录用户信息
+    user = g.user
+    if not user:
+        return redirect(url_for('index.index'))
+
+    context = {
+        'user': user
+    }
+
+    return render_template('news/user_base_info.html',context=context)
+
+
 @user_blue.route('/info',methods = ['GET','POST'])
 @user_login_data
 def user_info():
