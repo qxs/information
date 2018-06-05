7dea68180eaf20fd1067f4269deb25b44ea32f43
qxs <939727234@qq.com>
2018-06-05 17:21:56 +0800

    已登录管理员用户访问登录页面直接进入后台主页

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -4,14 +4,18 @@ from info.models import User
 
 @admin_blue.route('/')
 def admin_index():
+
     return render_template('admin/index.html')
-
-
 
 
 @admin_blue.route('/login',methods = ['GET','POST'])
 def admin_login():
     if request.method =='GET':
+        # 已登录用户访问登录页　直接进入主页面
+        user_id = session.get('user_id', None)
+        is_admin = session.get('is_admin', False)
+        if user_id and is_admin:
+            return redirect(url_for('admin.admin_index'))
         return render_template('admin/login.html')
     if request.method == "POST":
         username = request.form.get('username')
@@ -32,7 +36,7 @@ def admin_login():
         if not user.check_passowrd(password):
             return render_template('admin/login.html', errmsg='用户名或者密码错误')
 
-        session['user.id'] = user.id
+        session['user_id'] = user.id
         session['nick_name'] = user.nick_name
         session['mobile'] = user.mobile
         session['is_admin'] = user.is_admin
